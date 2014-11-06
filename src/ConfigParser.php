c879c8629b668345c2e0c6d8153b3611fc36280b
Stanislav Gamayunov <happyproff@gmail.com>
2014-11-07 01:34:55 +0500

    Refactored ConfigParser

--- src/ConfigParser.php
+++ src/ConfigParser.php
@@ -15,17 +15,29 @@ class ConfigParser implements ConfigParserInterface
      */
     public function parse($config)
     {
-        $configObject = new Config;
-
         if (!is_string($config)) {
             throw new InvalidConfigException('Config must be a string.');
         }
 
+        $configObject = new Config;
         $parts = explode(',', $config);
+        $this->setThumbnailParams($configObject, $parts[0]);
+        if (count($parts) > 1) {
+            $this->setParameters($configObject, array_slice($parts, 1));
+        }
 
+        return $configObject;
+    }
+
+    /**
+     * @param ConfigInterface $configObject
+     * @param string $thumbnailConfig
+     */
+    protected function setThumbnailParams(ConfigInterface $configObject, $thumbnailConfig)
+    {
         $thumbnailConfigParts = [];
-        if (preg_match('#(\d+)x(\d+)(:fit)*#', $parts[0], $thumbnailConfigParts) !== 1) {
-            throw new InvalidConfigException('Thumbnail config "' . $parts[0] . '" is incorrect.');
+        if (preg_match('#(\d+)x(\d+)(:fit)*#', $thumbnailConfig, $thumbnailConfigParts) !== 1) {
+            throw new InvalidConfigException('Thumbnail config "' . $thumbnailConfig . '" is incorrect.');
         }
         $configObject->setWidth(intval($thumbnailConfigParts[1]));
         $configObject->setHeight(intval($thumbnailConfigParts[2]));
@@ -36,9 +48,15 @@ class ConfigParser implements ConfigParserInterface
                 throw new InvalidConfigException('Thumbnail config modifier "' . $thumbnailConfigParts[2] . '" is incorrect. ');
             }
         }
+    }
 
-        if (count($parts) > 1) {
-            foreach (array_slice($parts, 1) as $parameter) {
+    /**
+     * @param ConfigInterface $configObject
+     * @param $parametersConfig
+     */
+    protected function setParameters (ConfigInterface $configObject, $parametersConfig) {
+
+        foreach ($parametersConfig as $parameter) {
                 $parameterParts = explode('=', $parameter);
                 $parameterName = $parameterParts[0];
                 $parameterValue = array_key_exists(1, $parameterParts) ? $parameterParts[1] : null;
@@ -54,8 +72,6 @@ class ConfigParser implements ConfigParserInterface
                         throw new InvalidConfigException('Parameter "' . $parameterName . '" is incorrect.');
                 }
             }
-        }
 
-        return $configObject;
     }
 }
